4f6c0d9f3c12f5ca976cb87baad342b729d0b258
Henry Tum <tujungahenry@gmail.com>
2020-04-04 13:54:21 -0700

    running build

---
+++ client/build/precache-manifest.20b60f8836c95e5283b29aff04e51e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ddbc28ae3cde67ca1eed",
+    "url": "/static/js/main.ddbc28ae.chunk.js"
+  },
+  {
+    "revision": "bff80cf3fc5b8f731273",
+    "url": "/static/js/1.bff80cf3.chunk.js"
+  },
+  {
+    "revision": "ddbc28ae3cde67ca1eed",
+    "url": "/static/css/main.dd72b52a.chunk.css"
+  },
+  {
+    "revision": "bff80cf3fc5b8f731273",
+    "url": "/static/css/1.b347ab62.chunk.css"
+  },
+  {
+    "revision": "fc1910bdb817b95d321dc91c3b275e0f",
+    "url": "/index.html"
+  }
+];
